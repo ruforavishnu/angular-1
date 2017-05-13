b8f31d2a5ddc4183b84b90e75d3c97314ba5a1b3
efghTech <vishnuajith310@gmail.com>
2017-05-13 13:02:13 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import { DataService} from './data.service'; 
+import {trigger,state,style,transition,animate,keyframes} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>The web app of html</h1>
+
+  <p>Begin web app</p>
+  
+  
+
+  <button [@myAwesomeAnimation]='state' (click)="animateMe()"> I'm an animatable button</button>
+  `,
+  styles: [`
+	button {
+		width:200px;
+		background:lightgrey;
+		margin:200px auto;
+		text-align: center;
+		padding:20px;
+		font-size:1.5em;
+
+	}
+  `],
+  	animations: [
+  	trigger('myAwesomeAnimation',
+  		[
+  			state('small',style(
+  			{
+  				transform:'scale(1)'
+
+  			})),
+  			state('large',style(
+  			{
+  				transform:'scale(1.2)'
+
+  			})),
+  			transition('small <=> large', animate('300ms ease-in', style({
+  				transform:'translateY(40px)'
+  			}))),
+
+  		]),
+  	]
+  
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent 
+{
+state:string = 'small';
+
+  constructor(private dataService:DataService)
+{ }
+	
+
+	animateMe() 
+	{
+		this.state = (this.state==='small'? 'large':'small');
+	}
+
+
+  
 }
